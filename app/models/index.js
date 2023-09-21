737f97de2a261b3e2cf8570000a74c93b59759e5
quang <nguyenthequang080203@gmail.com>
2023-09-21 08:34:26 +0700

    establish db relationships

--- app/models/index.js
+++ app/models/index.js
@@ -19,6 +19,28 @@ const db = {};
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
-db.tutorials = require("./tutorialModels")(sequelize, Sequelize);
+db.tutorials = require("./tutorialModel")(sequelize, Sequelize);
+db.comments = require("./commentModel")(sequelize, Sequelize);
+db.tags = require("./tagModel")(sequelize, Sequelize);
+
+//one-many relationship
+db.tutorials.hasMany(db.comments, { as: "comments" });
+db.comments.belongsTo(db.tutorials, {
+  foreignKey: "tutorialId",
+  as: "tutorial",
+});
+
+//many-many relationship: One tut has many tags and one tag points to many tuts
+db.tags.belongsToMany(db.tutorials, {
+  through: "tutorial_tag",
+  as: "tutorials",
+  foreignKey: "tag_id",
+});
+
+db.tutorials.belongsToMany(db.tags, {
+  through: "tutorial_tag",
+  as: "tags",
+  foreignKey: "tutorial_id",
+});
 
 module.exports = db;
